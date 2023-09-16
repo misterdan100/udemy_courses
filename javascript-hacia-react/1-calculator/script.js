182b4e26277a5385d1461c1c51ff215b33545d35
misterdan100 <danielmca60@gmail.com>
2023-09-15 23:27:02 -0500

    Making project 1 Calculator JavaScript y sass

---
+++ javascript-hacia-react/1-calculator/script.js
@@ -0,0 +1,34 @@
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
+
+//* recorrer el arreglo de botones que crear querySelectorAll
+buttons.forEach(item => {
+    item.onclick=()=>{
+        if(item.id === 'clear') {
+            display.innerText = "";
+        } else if (item.id === 'backspace') {
+            let string = display.innerText.toString();
+            display.innerHTML = string.substr(0, string.length - 1);
+        } else if (display.innerText !== '' && item.id === 'equal') {
+            display.innerText = eval(display.innerText);
+        } else if (display.innerText === '' && item.id === 'equal') {
+            display.innerText = 'Null';
+            setTimeout(() => {
+                display.innerHTML = '';
+            }, 2000);
+        } else {
+            display.innerText += item.id;
+        }
+    }
+})
+
+//* Activar Dark Theme
+const themeToggleBtn = document.querySelector('.theme-toggler');
+const calculator = document.querySelector('.calculator');
+
+let isDark = true;
+themeToggleBtn.onclick=()=>{
+    calculator.classList.toggle('dark')
+    themeToggleBtn.classList.toggle('active')
+    isDark=!isDark;
+}
