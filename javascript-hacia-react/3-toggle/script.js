e5ad4bff18505ddec837998f5327aee51ffe06f5
misterdan100 <danielmca60@gmail.com>
2023-09-18 21:46:43 -0500

    Making project 3 toggle with JS

---
+++ javascript-hacia-react/3-toggle/script.js
@@ -0,0 +1,6 @@
+let toggleBtn = document.querySelector('.toggleBtn');
+let container = document.querySelector('.container');
+let content = document.querySelector('.content');
+
+toggleBtn.onclick = () => {container.classList.toggle('active')};
+
